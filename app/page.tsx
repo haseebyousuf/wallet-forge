f367437f866bea66289edf00bc6446dd038744bb
Haseeb <haseeb@dezyit.com>
2024-08-15 18:50:50 +0530

    chore: Add react-hot-toast toaster component for notifications

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { GenerateWallet } from "@/components/GenerateWallet";
 import Header from "@/components/Header";
 import WalletList from "@/components/WalletList";
 import { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   const [wallets, setWallets] = useState<Wallet[]>([]);
@@ -14,6 +15,7 @@ export default function Home() {
   }, []);
   return (
     <main className="flex w-screen min-h-screen flex-col items-center gap-2 ">
+      <Toaster position="top-right" />
       <Header />
       <GenerateWallet wallets={wallets} setWallets={setWallets} />
       <WalletList wallets={wallets} setWallets={setWallets} />
